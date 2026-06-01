8126fd09a362e201adc699c77fe54518b9129890
Srivastavarachit01 <rachitsrivastava1845@gmail.com>
2026-06-02 02:04:30 +0530

    Array Construct solution

---
+++ A_Construct_an_Array.cpp
@@ -0,0 +1,13 @@
+#include <iostreamg>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+            cout << (2*i+1) << " \n"[i==n-1];
+    }
+}

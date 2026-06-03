a4d87ebc7fa81154dfc58ecc470ff758145eda77
Srivastavarachit01 <rachitsrivastava1845@gmail.com>
2026-06-03 23:48:20 +0530

    2last digit soln

---
+++ Y_The_last_2_digits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    long long A, B, C, D;
+    cin >> A >> B >> C >> D;
+ 
+    long long result = ((A % 100) * (B % 100) % 100 * (C % 100) % 100 * (D % 100)) % 100;
+ 
+    if (result < 10)
+        cout << "0" << result;
+    else
+        cout << result;
+ 
+    return 0;
+}

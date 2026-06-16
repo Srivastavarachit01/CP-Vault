1bad240085abd965f008b179b1ed5e5de2f6167c
Rachit Srivastava <rachitsrivastava1845@gmail.com>
2026-06-16 23:45:28 +0530

    A Soldier and Bananas Solutions

---
+++ A_Soldier_and_Bananas.cpp
@@ -0,0 +1,13 @@
+#include <iostream>>
+using namespace std;
+
+int main() {
+    long long k, n, w;
+    cin >> k >> n >> w;
+    
+    long long cost = k * w * (w + 1) / 2;
+    long long borrow = max(0LL, cost - n);
+    
+    cout << borrow << endl;
+    return 0;
+}

e6a5673e8adcc4d8f853c0ec40a5975c0afdbc80
Rachit Srivastava <rachitsrivastava1845@gmail.com>
2026-06-22 23:57:09 +0530

    Anton and Danik Solution

---
+++ Anton and Danik.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    string s;
+    cin >> n >> s;
+    
+    int a = count(s.begin(), s.end(), 'A');
+    int d = n - a;
+    
+    if(a > d) cout << "Anton";
+    else if(d > a) cout << "Danik";
+    else cout << "Friendship";
+    
+    return 0;
+}

e99c2209c878b516df433146775407628d3d782b
Rachit Srivastava <rachitsrivastava1845@gmail.com>
2026-06-23 23:58:40 +0530

    vanya and fence solution

---
+++ Vanya and Fence.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n, h;
+    cin >> n >> h;
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        int a;
+        cin >> a;
+        if(a <= h) ans += 1;
+        else ans += 2;
+    }
+    cout << ans << endl;
+}

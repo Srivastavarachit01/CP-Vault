eeda88c74b5356581346dfad7ec5543665120c2f
Rachit Srivastava <rachitsrivastava1845@gmail.com>
2026-06-28 23:43:17 +0530

    Chat room solution

---
+++ Chat room.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s, t = "hello";
+    cin >> s;
+    int j = 0;
+    for(int i = 0; i < s.size() && j < 5; i++)
+        if(s[i] == t[j]) j++;
+    cout << (j == 5 ? "YES" : "NO");
+}
